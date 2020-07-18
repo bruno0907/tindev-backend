c5a636f96c5e28529a5e3866268161bbbbda4590
Bruno Mariani <bruno0907@gmail.com>
2020-07-17 23:00:17 -0300

    Socket.io implementation

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
 const cors = require('cors')
 require('dotenv').config()
 
 const routes = require('./routes')
 
-const server = express();
+const app = express();
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+})
 
 const PORT = process.env.PORT || 3333;
 const DB = process.env.DB;
@@ -20,9 +31,16 @@ mongoose.connect(DB, {
     console.error('Database connection failed!')
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(PORT, function() {
     console.log(`Server Running on ${this.address().port} port `);
